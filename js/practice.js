17dac3287ba1658e7a5849cdca349b953c38eb54
Marius <marius@example.com>
2022-10-26 15:16:50 +0300

    changeTextContent

--- js/practice.js
+++ js/practice.js
@@ -6,14 +6,27 @@ const proverb =
 
 const htmlEls = {
   h1: document.querySelector('h1'),
+  sent1: document.querySelector('#sent1'),
 };
 // const h1El = document.querySelector('h1');
 
 function changeH1() {
   const currentText = htmlEls.h1.textContent; //String methods
-  const changedText = currentText.replace(/MeThods/i, 'metodai');
+  const changedText = currentText.replace('methods', 'metodai');
   // irasyti atgal i elementa
   htmlEls.h1.textContent = changedText;
 }
 
 changeH1();
+
+function changeText() {
+  const currentText = htmlEls.sent1.textContent;
+  const changedText = currentText.replace('until we', 'we just');
+  htmlEls.sent1.textContent = changedText;
+}
+changeText();
+
+changeTextContent(htmlEls.h1, 'methods', 'metodai');
+changeTextContent(htmlEls.sent1, 'until we', 'we just');
+
+function changeTextContent() {}
